c8074ff5c74eaed0d350ed1a789ced435f35a988
Salvo1789 <salvo.mercurio89@gmail.com>
2023-03-14 17:25:13 +0100

    modified functions ex1

--- ex-1/function.js
+++ ex-1/function.js
@@ -2,26 +2,31 @@ function getName() {
     return localStorage.getItem("userName");
   }
   
-  function updateHTML() {
-    var name = getName();
-  }
+function updateHTML() {
+  var name = getName();
+}
   
-  function saveName() {
-    // Gets input value
-    var name = document.getElementById("yourName").value;
+function saveName() {
+  // Gets input value
+  var name = document.getElementById("yourName").value;
   
-    // Saves data to retrieve later
-    localStorage.setItem("userName", name);
-    
-    // Updates HTML
-    updateHTML();
-  }
+   // Saves data to retrieve later
+  localStorage.setItem("userName", name);
+  
+  var text = document.getElementById("savedData");
+  text.innerText = localStorage.getItem("userName");
+  // Updates HTML
+  updateHTML();
+}
 
-  function removeName() {
+function removeName() {
+
+  var text = document.getElementById("savedData");  
+  text.innerText = "";
+  // Saves data to retrieve later
+  localStorage.removeItem("userName");
+
     
-    // Saves data to retrieve later
-    localStorage.removeItem("userName");
-    
-    // Updates HTML
-    updateHTML();
-  }
+   // Updates HTML
+  updateHTML();
+}
--- ex-2/timer.js
+++ ex-2/timer.js
@@ -1,13 +1,12 @@
 var counter = null;
 var counters = null;
 
-do{
+
 if(sessionStorage.getItem("counter") == null){
   counter=sessionStorage.setItem("count", 1);
   counters = 1;
 }else{
-  //counters= parseInt(sessionStorage.getItem("count")); 
   counters++;
   counter=sessionStorage.setItem("count", counters);
 }
-}while(counters>1)
+
